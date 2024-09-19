d00b365baa5a1ed0d4d2a945be8cb686ec807a6d
revestskiy <andrey.billi2000@bk.ru>
2024-09-19 20:52:35 +0300

    Screens

---
+++ app/src/main/java/com/SkyFlyGame/SkywardTreasure/MenuScreen.kt
@@ -0,0 +1,73 @@
+package com.SkyFlyGame.SkywardTreasure
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun MenuScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painter = painterResource(id = R.drawable.background),
+                contentScale = ContentScale.Crop
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 80.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.playbutton),
+                contentDescription = "Start Button",
+                modifier = Modifier
+                    .size(width = 200.dp, height = 100.dp)
+                    .clickable {
+
+                    }
+            )
+            Image(
+                painter = painterResource(id = R.drawable.settingsbutton),
+                contentDescription = "Start Button",
+                modifier = Modifier
+                    .size(width = 200.dp, height = 100.dp)
+                    .clickable {
+
+                    }
+            )
+
+            Spacer(modifier = Modifier.height(52.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.exitbutton),
+                contentDescription = "Exit Button",
+                modifier = Modifier
+                    .size(width = 150.dp, height = 80.dp)
+                    .clickable {
+
+                    }
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/SkyFlyGame/SkywardTreasure/SettingsScreen.kt
@@ -0,0 +1,137 @@
+package com.SkyFlyGame.SkywardTreasure
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import com.SkyFlyGame.SkywardTreasure.ui.theme.nujnoefont
+
+@Preview
+@Composable
+fun SettingsScreen() {
+    var musicVolume by remember { mutableStateOf(0.5f) }
+    var soundVolume by remember { mutableStateOf(0.5f) }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Image(
+            painter = painterResource(id = R.drawable.settingbackground),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .size(300.dp)
+
+        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center)
+                .padding(32.dp)
+        ) {
+
+
+            TextWithShadowAndOutline(
+                text = "Settings",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+
+            TextWithShadowAndOutline(
+                text = "Music",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Slider(
+                value = musicVolume,
+                onValueChange = { musicVolume = it },
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+
+            TextWithShadowAndOutline(
+                text = "Sound",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Slider(
+                value = soundVolume,
+                onValueChange = { soundVolume = it },
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+        }
+
+        // Кнопка "Back"
+        Image(
+            painter = painterResource(id = R.drawable.backbutton),
+            contentDescription = "",
+            modifier = Modifier
+                .size(100.dp)
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 32.dp)
+                .clickable {
+
+                }
+        )
+    }
+}
+
+@Composable
+fun TextWithShadowAndOutline(
+    text: String,
+    fontSize: TextUnit,
+    fontWeight: FontWeight
+) {
+
+    Text(
+        text = text,
+        fontFamily = nujnoefont,
+        fontSize = fontSize,
+        fontWeight = fontWeight,
+        color = Color.White,
+        style = TextStyle(
+            shadow = Shadow(
+                color = Color.Black,
+                offset = Offset(5f, 5f),
+                blurRadius = 2.5f
+            )
+        )
+    )
+}
+
